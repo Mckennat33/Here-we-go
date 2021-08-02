1a15ba8debcb99b839821ca30c77dd67044caa4a
thomasmckenna <thomasmckenna12@gmail.com>
2021-08-02 13:48:31 -0400

    first commit, files created

--- script.js
+++ script.js
@@ -1,4 +1,13 @@
-const h2 = document.createElement("h2");
-h2.textContent = "This content added by JavaScript"; 
+// const h2 = document.createElement("h2");
+// h2.textContent = "This content added by JavaScript"; 
+// 
+// document.querySelector("body").appendChild(h2); 
 
-document.querySelector("body").appendChild(h2); 
+
+
+
+myFunction()
+
+function myFunction () {
+    document.getElementById("demo").innerHTML = "Hurry up! Subscibe to THE Blog!"
+}
